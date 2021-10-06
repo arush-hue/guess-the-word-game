dc765abe87a942c398e1219df934d6f4fd25b682
arush-hue <77105133+arush-hue@users.noreply.github.com>
2021-10-06 19:45:51 +0530

    Add files via upload

---
+++ game_login.js
@@ -0,0 +1,8 @@
+function update()
+{
+    player1_name=document.getElementById("hue").value;
+    player2_name=document.getElementById("hue2").value;
+    localStorage.setItem("player1_name",player1_name);
+    localStorage.setItem("player2_name",player2_name);
+    window.location="game_page.html";
+}
